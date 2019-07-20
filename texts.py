362e3944345ae0b60576f5ea8e39b34a262af7e1
Urhengulas <joheihe@web.de>
2019-07-20 15:06:12 +0200

    Change texts

--- texts.py
+++ texts.py
@@ -1,2 +1,17 @@
-START_TEXT = "Hello I am Fwdbot. I forward your Telegram messages to a secret admin channel, so they can do their filthy stuff with it ;D\n\nYour chat id: {}"
-THANK_TEXT = "Thank you {}, for sending a message."
+from emoji import emojize
+
+START_TEXT = emojize(
+    """Hallo!
+Ich bin der Blitzer-Bot :rotating_light: für den Landkreis RT. Gerne kannst Du mir eine Nachricht schreiben, die ich dann an die Administratoren :busts_in_silhouette: weiterleite. Bitte beschreibe möglichst präzise aber kurz und in einer einzigen Nachricht, wo der Blitzer :camera: steht :blush:
+
+Deine chat id: {}
+""",
+    use_aliases=True
+)
+
+THANK_TEXT = emojize(
+    """Vielen Dank!
+Nach kurzer Prüfung werden die Admins den Blitzer in den Channel https://t.me/BlitzerKreisRT Posten. Schau doch direkt Mal rein und abonniere uns!
+""",
+    use_aliases=True
+)
